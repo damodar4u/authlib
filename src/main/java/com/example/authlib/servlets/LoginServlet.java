42f9efe76582159f9450fe3ad9f1a085135570b8
Damodar <damodar.neelapu@yahoo.com>
2024-12-03 10:26:31 -0500

    Create LoginServlet.java

---
+++ src/main/java/com/example/authlib/servlets/LoginServlet.java
@@ -0,0 +1,22 @@
+package com.example.authlib.servlets;
+
+import com.example.authlib.config.AuthConfig;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.URLEncoder;
+
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        AuthConfig config = (AuthConfig) getServletContext().getAttribute("authConfig");
+        String loginUrl = "https://login.microsoftonline.com/{tenant}/oauth2/v2.0/authorize" +
+                "?client_id=" + URLEncoder.encode(config.getClientId(), "UTF-8") +
+                "&response_type=code" +
+                "&redirect_uri=" + URLEncoder.encode(config.getRedirectUri(), "UTF-8") +
+                "&scope=openid profile email";
+        resp.sendRedirect(loginUrl);
+    }
+}
