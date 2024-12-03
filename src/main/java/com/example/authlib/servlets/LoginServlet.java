481f619c04fdfb6d6bf6b40dee832ddfc0c368d8
Damodar <damodar.neelapu@yahoo.com>
2024-12-03 10:43:37 -0500

    Update LoginServlet.java

--- src/main/java/com/example/authlib/servlets/LoginServlet.java
+++ src/main/java/com/example/authlib/servlets/LoginServlet.java
@@ -12,11 +12,15 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         AuthConfig config = (AuthConfig) getServletContext().getAttribute("authConfig");
+
+        // Construct the Azure AD authorization URL
         String loginUrl = "https://login.microsoftonline.com/{tenant}/oauth2/v2.0/authorize" +
                 "?client_id=" + URLEncoder.encode(config.getClientId(), "UTF-8") +
                 "&response_type=code" +
                 "&redirect_uri=" + URLEncoder.encode(config.getRedirectUri(), "UTF-8") +
                 "&scope=openid profile email";
+
+        // Redirect the user to Azure AD login page
         resp.sendRedirect(loginUrl);
     }
 }
